35fe99f4856c9c6d46dc538791e62b50a2b2e1cb
oscarotero <oom@oscarotero.com>
2016-12-06 18:08:49 +0100

    updated tests

--- tests/RecaptchaTest.php
+++ tests/RecaptchaTest.php
@@ -12,9 +12,9 @@ class RecaptchaTest extends \PHPUnit_Framework_TestCase
     {
         $request = Factory::createServerRequest([], 'POST');
 
-        $response = (new Dispatcher([
+        $response = Dispatcher::run([
             new Recaptcha(uniqid()),
-        ]))->dispatch($request);
+        ], $request);
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
         $this->assertEquals(403, $response->getStatusCode());
