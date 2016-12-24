6ca9dacab9dc0294c4a201dd6a697db77af33984
oscarotero <oom@oscarotero.com>
2016-12-24 10:42:12 +0100

    Updated http-middleware interface to 0.4 and php-cs-fixer to 2.0

--- src/Recaptcha.php
+++ src/Recaptcha.php
@@ -4,11 +4,11 @@ namespace Middlewares;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use Interop\Http\Middleware\ServerMiddlewareInterface;
-use Interop\Http\Middleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Interop\Http\ServerMiddleware\DelegateInterface;
 use ReCaptcha\ReCaptcha as GoogleRecaptcha;
 
-class Recaptcha implements ServerMiddlewareInterface
+class Recaptcha implements MiddlewareInterface
 {
     /**
      * @var string
